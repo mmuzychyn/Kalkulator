4e61d43f1884ae63adc98f392775a9913bc4c201
RudekAlone <44509433+RudekAlone@users.noreply.github.com>
2025-02-06 14:59:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,125 @@
+const display = document.querySelector('.calculator-screen');
+
+const buttons = document.querySelectorAll(".calculator-keys>button");
+
+let buttonNum = [];
+let buttonOperator = [];
+let buttonFunction = [];
+let cache = [];
+let cacheValue =  "";
+buttons.forEach((button) => {
+    if(button.classList.contains('operator')) {
+        buttonOperator.push(button);
+        const operator = button.value;
+        switch(operator) {
+            case '+':
+                button.addEventListener('click', (e) => {
+                    if (cacheValue !== "") {
+                        add(parseFloat(cacheValue));
+                    }
+                });
+                break;
+            case '-':
+                button.addEventListener('click', (e) => {
+                    if (cacheValue !== "") {
+                        subtract(parseFloat(cacheValue));
+                    }
+                });
+                break;
+            case '*':
+                button.addEventListener('click', (e) => {
+                    // mnożenie
+                });
+                break;
+            case '/':
+                button.addEventListener('click', (e) => {
+                    // dzielenie
+                });
+                break;
+        }
+    }else if(button.classList.contains('decimal')) {
+        
+    }else if(button.classList.contains('all-clear')) {
+        buttonFunction.push(button);
+        button.addEventListener('click', (e) => {
+            clearDisplay();
+            cache = [];
+        });
+    }else if(button.classList.contains('equal-sign')) {
+        buttonFunction.push(button);
+        button.addEventListener('click', (e) => {
+            equal();
+        });
+    }else {
+        buttonNum.push(button);
+        buttonFunction.push(button);
+        button.addEventListener('click', (e) => {
+            setDisplayValue(e.target.value);
+            console.log(e.target.value);
+        });
+    }
+
+});
+
+
+function setDisplayValue(value) {
+    
+    display.innerText += value;
+    console.log("value:"+value)
+    cacheValue += value;
+}
+function clearDisplay() {
+    display.innerText = "";
+    cacheValue = "";
+}
+let test = true;
+function add(a) {
+    cache.push(a);
+    console.log(cache);
+    if(cache.length >= 2) {
+        clearDisplay();
+        let sum = cache[0] + cache[1];
+        cache = [];
+        cache.push(sum);
+        setDisplayValue(cache);
+    }
+    else {
+        clearDisplay();
+    }
+}
+function subtract(a) {
+    cache.push(a);
+    console.log(cache);
+    if(cache.length >= 2) {
+        clearDisplay();
+        let sum = cache[0] - cache[1];
+        cache = [];
+        cache.push(sum);
+        setDisplayValue(cache);
+    }
+    else {
+        clearDisplay();
+    }
+
+
+}
+function equal(){
+    // Sprawdzenie jaki operator został wybrany jako ostatni i czy została podana liczba wtedy wykonaj działanie ostatniego operatora
+    // Jeżeli nie podano liczby a kliknieto operator to wyświetl wartość z cache
+}
+
+// metoda/funkcja mnożenia
+
+// metoda/funkcja dzielenia
+
+// metoda/funkcja liczb zmiennoprzecinkowych dodawany jest przecinek a wartości float muszą zawierać kropkę np. 1.2 zamiast 1,2
+
+//Te zmienne nie są wykorzystywane. Jedynie dodaje się do nich przyciski z kalkulatora i potem nie są wykorzystywane
+// pasowałoby je usunąć z kodu
+// let buttonNum = [];
+// let buttonOperator = [];
+// let buttonFunction = [];
+
+// Gdy wszystko bedzie działać dopisz komentarze wyjaśniające działanie kodu oraz udokumentuj go w plikach Markdown dokumentacji:
+// https://github.com/Code-V-Craft/Documentation
+// Ten kod powinien być w Moduł 0: Kalkulator
